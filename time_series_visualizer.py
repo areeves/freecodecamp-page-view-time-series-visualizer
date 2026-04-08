21dae39c6fcc310118749c4c11e4b9d76ee50704
Andrew Reeves <areeves42@gmail.com>
2026-04-07 21:15:55 -0400

    draw_bar_plot()

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -3,6 +3,7 @@ import pandas as pd
 import seaborn as sns
 from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
+import datetime
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
 df = pd.read_csv('fcc-forum-pageviews.csv', index_col='date', parse_dates=True)
@@ -24,14 +25,36 @@ def draw_line_plot():
 
 def draw_bar_plot():
     # Copy and modify data for monthly bar plot
-    df_bar = None
+    df_bar = df.resample('ME').mean().reset_index()
+    df_bar["year"] = df_bar.iloc[:, 0].dt.year
+    df_bar["month"] = df_bar.iloc[:, 0].dt.strftime("%b")
 
     # Draw bar plot
-
-
-
-
-
+    g = sns.catplot(
+        data=df_bar,
+        x="year",
+        y=df_bar.columns[1],      # the 'value' column after resampling
+        hue="month",
+        hue_order=["Jan", "Feb", "Mar", "Apr", "May", "Jun","Jul","Aug","Sep","Oct","Nov","Dec"],
+        kind="bar",
+        # height=figsize[1] / 1.2,   # approximate aspect control
+        # aspect=figsize[0] / figsize[1],
+        palette="tab20",
+        legend_out=True
+    )
+    
+    # Access the underlying figure and axes for OO control
+    fig = g.fig
+    ax = g.ax
+    
+    # Minimal customization
+    ax.set_xlabel("Year")
+    ax.set_ylabel('Page Views Per Month')
+    ax.set_title('FCC Forum Page Views')
+    
+    # Improve legend
+    g.legend.set_title("Month")
+    
     # Save image and return fig (don't change this part)
     fig.savefig('bar_plot.png')
     return fig
